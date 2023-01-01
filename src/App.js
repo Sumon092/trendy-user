875daf31ecbca1ff0067f77cfe84aca684ddfed0
SUMON <m.sumon@outlook.com>
2023-01-02 04:00:20 +0600

    all ten user card complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
-import ShowUser from "./components/Home/ShowUser"
 import Cards from './components/Home/Card';
-
 
 function App() {
   return (
     <div>
       <Cards />
-      <ShowUser />
+
     </div>
   );
 }
--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -1,5 +1,6 @@
-
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../../features/users/userSlice';
 import { MailOutlined, PhoneOutlined, DeleteOutlined, HeartOutlined, EditFilled, GlobalOutlined } from '@ant-design/icons';
 import { Button, Modal, Card, Row, Col, Form, Input } from 'antd';
 import { useState } from 'react';
@@ -7,6 +8,12 @@ import './Card.css'
 
 
 const Cards = () => {
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.user);
+    console.log(user);
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
     const [isModalOpen, setIsModalOpen] = useState(false);
     const showModal = () => {
         setIsModalOpen(true);
@@ -28,111 +35,116 @@ const Cards = () => {
         <section>
             <Row gutter={[16, 16
             ]}>
-                <Col span={6} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8 }} lg={{ span: 6 }}>
-                    <div className='container'>
-                        <div className='image-container'>
-                            <div className='avatar'>
-                                <img src="https://avatars.dicebear.com/v2/avataaars/Karianne.svg?options[mood][]=happy" alt="" />
-                            </div>
-                        </div>
+                {
+                    user.users.map((user) => (
+                        // <li key={user.id}>{user.name}</li>
+                        <Col span={6} xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 8 }} lg={{ span: 6 }}>
+                            <div className='container'>
+                                <div className='image-container'>
+                                    <div className='avatar'>
+                                        <img src="https://avatars.dicebear.com/v2/avataaars/Karianne.svg?options[mood][]=happy" alt="" />
+                                    </div>
+                                </div>
 
-                        <Card style={{ borderRadius: "0" }}>
-                            <div className='card-info'>
-                                <h3>Earling Haland</h3>
-                                <div className='info-detail'>
-                                    <MailOutlined />
-                                    <p>emsl.sumon@gmail.com</p>
-                                </div>
-                                <div className='info-detail'>
-                                    <PhoneOutlined />
-                                    <p>01916231926</p>
-                                </div>
-                                <div className='info-detail'>
-                                    <GlobalOutlined />
-                                    <p>www.asp.net</p>
-                                </div>
-                            </div>
-                        </Card>
-                        <div className='button-container'>
-                            <div className='button'>
-                                <Button style={{ border: "none", background: 'none', boxShadow: 'none', color: 'red' }} icon={<HeartOutlined />}></Button>
-                            </div>
+                                <Card style={{ borderRadius: "0" }}>
+                                    <div className='card-info'>
+                                        <h3>{user.name}</h3>
+                                        <div className='info-detail'>
+                                            <MailOutlined />
+                                            <p>{user.email}</p>
+                                        </div>
+                                        <div className='info-detail'>
+                                            <PhoneOutlined />
+                                            <p>{user.phone}</p>
+                                        </div>
+                                        <div className='info-detail'>
+                                            <GlobalOutlined />
+                                            <p>{user.website}</p>
+                                        </div>
+                                    </div>
+                                </Card>
+                                <div className='button-container'>
+                                    <div className='button'>
+                                        <Button style={{ border: "none", background: 'none', boxShadow: 'none', color: 'red' }} icon={<HeartOutlined />}></Button>
+                                    </div>
 
-                            <div className='button'>
-                                <Button onClick={showModal} style={{ border: "none", background: 'none', boxShadow: 'none' }} icon={<EditFilled />}></Button>
-                                <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                                    <Form
-                                        name="basic"
-                                        labelCol={{
-                                            span: 8,
-                                        }}
-                                        wrapperCol={{
-                                            span: 16,
-                                        }}
-                                        initialValues={{
-                                            remember: true,
-                                        }}
-                                        onFinish={onFinish}
-                                        onFinishFailed={onFinishFailed}
-                                        autoComplete="off"
-                                    >
-                                        <Form.Item
-                                            label="name"
-                                            name="name"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    message: 'Please input your name!',
-                                                },
-                                            ]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                        <Form.Item
-                                            label="email"
-                                            name="email"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    message: 'Please input your email!',
-                                                },
-                                            ]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                        <Form.Item
-                                            label="phone"
-                                            name="phone"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    message: 'Please input your phone no.!',
-                                                },
-                                            ]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                        <Form.Item
-                                            label="website"
-                                            name="website"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    message: 'Please input your website!',
-                                                },
-                                            ]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                    </Form>
-                                </Modal>
+                                    <div className='button'>
+                                        <Button onClick={showModal} style={{ border: "none", background: 'none', boxShadow: 'none' }} icon={<EditFilled />}></Button>
+                                        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                                            <Form
+                                                name="basic"
+                                                labelCol={{
+                                                    span: 8,
+                                                }}
+                                                wrapperCol={{
+                                                    span: 16,
+                                                }}
+                                                initialValues={{
+                                                    remember: true,
+                                                }}
+                                                onFinish={onFinish}
+                                                onFinishFailed={onFinishFailed}
+                                                autoComplete="off"
+                                            >
+                                                <Form.Item
+                                                    label="name"
+                                                    name="name"
+                                                    rules={[
+                                                        {
+                                                            required: true,
+                                                            message: 'Please input your name!',
+                                                        },
+                                                    ]}
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+                                                <Form.Item
+                                                    label="email"
+                                                    name="email"
+                                                    rules={[
+                                                        {
+                                                            required: true,
+                                                            message: 'Please input your email!',
+                                                        },
+                                                    ]}
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+                                                <Form.Item
+                                                    label="phone"
+                                                    name="phone"
+                                                    rules={[
+                                                        {
+                                                            required: true,
+                                                            message: 'Please input your phone no.!',
+                                                        },
+                                                    ]}
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+                                                <Form.Item
+                                                    label="website"
+                                                    name="website"
+                                                    rules={[
+                                                        {
+                                                            required: true,
+                                                            message: 'Please input your website!',
+                                                        },
+                                                    ]}
+                                                >
+                                                    <Input />
+                                                </Form.Item>
+                                            </Form>
+                                        </Modal>
+                                    </div>
+                                    <div className='button' style={{ border: 'none' }}>
+                                        <Button style={{ border: "none", background: 'none', boxShadow: 'none' }} icon={<DeleteOutlined />}></Button>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='button' style={{ border: 'none' }}>
-                                <Button style={{ border: "none", background: 'none', boxShadow: 'none' }} icon={<DeleteOutlined />}></Button>
-                            </div>
-                        </div>
-                    </div>
-                </Col>
+                        </Col>
+                    ))}
+
             </Row>
 
         </section>
